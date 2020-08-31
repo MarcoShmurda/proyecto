09493f506cb88b150c2455fc72ef83ffa31dfff9
Marco Shmurda <marcoestevezpetit01@gmail.com>
2020-08-31 01:00:08 -0400

    Tooltip y logo

--- js/index.js
+++ js/index.js
@@ -12,3 +12,16 @@ let iconUser = document.querySelector('.fas.fa-user').addEventListener('click',(
     let boxSign = document.querySelector('.box-log-in')
     boxSign.classList.toggle('mover-log-in')
 })
+
+
+
+var tooltips = document.querySelectorAll('.tooltip span');
+
+window.onmousemove = function (e) {
+    var x = (e.clientX + 20) + 'px',
+        y = (e.clientY + 20) + 'px';
+    for (var i = 0; i < tooltips.length; i++) {
+        tooltips[i].style.top = y;
+        tooltips[i].style.left = x;
+    }
+};
